aea6a78de57995c2b8a6ab9fae33eec701c350ce
Paolo Ragudo <ragudopaolo@gmail.com>
2026-02-03 16:12:18 +0800

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,33 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.port ?? 3000;
+const __dirname = path.resolve();
 
 // middleware
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
+
 app.use(express.json());
 app.use(rateLimiter); // try later at home connecting to the app via phone, and test the rate limit if it applies to one IP or all IPs 
 
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
